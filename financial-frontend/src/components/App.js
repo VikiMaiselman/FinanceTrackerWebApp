3cda865ecf3ed9748a6be63f5011267905ee5827
VikiMaiselman <maiselmanwikki@gmail.com>
2024-01-28 12:47:22 +0200

    remove unnecessaty imports from App.js

--- financial-frontend/src/components/App.js
+++ financial-frontend/src/components/App.js
@@ -1,14 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../styles/App.css";
 import AppRouter from "./AppRouter";
-import Login from "./forms/Login";
-
-import axios from "axios";
-const url = "http://localhost:3007/authstatus";
-const headers = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "http://localhost:3000",
-};
 
 function App() {
   return (
